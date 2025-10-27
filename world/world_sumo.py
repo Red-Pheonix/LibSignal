19b47be3630657e3717ace610675d988cdfb7f13
Red-Pheonix <rafirafi155@gmail.com>
2025-10-27 15:26:08 +0600

    hack

--- world/world_sumo.py
+++ world/world_sumo.py
@@ -419,11 +419,11 @@ class World(object):
         self.green_phases = self.generate_valid_phase()
         # g_phases = {k:[(phase.duration,phase.state) for phase in phases] for  k,phases in self.green_phases.items()}
         # self.eng.trafficlight.Phase(10, "r")
-        # with open("tempe.pkl", "wb") as f:
-        #     pickle.dump(g_phases, f)
-        with open("tempe.pkl", "rb") as f:
-            g_phases = pickle.load(f)
-            self.green_phases = {k:[self.eng.trafficlight.Phase(phase[0], phase[1]) for phase in phases] for  k,phases in g_phases.items()}
+        with open("tempe.pkl", "wb") as f:
+            pickle.dump(g_phases, f)
+        # with open("tempe.pkl", "rb") as f:
+            # g_phases = pickle.load(f)
+            # self.green_phases = {k:[self.eng.trafficlight.Phase(phase[0], phase[1]) for phase in phases] for  k,phases in g_phases.items()}
     
         # creating all intersections
         self.id2intersection = dict()
